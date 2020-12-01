5795a6580c1c731b762191437bacfdc4dea85d8b
Aleksandr Šmailov <asa@solutionlab.net>
2020-12-01 17:21:33 +0200

    Added code coverage badge

--- index.ts
+++ index.ts
@@ -1,31 +1,5 @@
 import { useState as useReactState } from "react";
-
-type UseState = typeof useReactState;
-
-const createUseFlag = (useState: UseState) =>
-  function useFlag<Name extends string>(name: Name, initial: boolean) {
-    const [flag, setFlag] = useState<boolean>(initial);
-
-    const toggle = () => setFlag((value) => !value);
-    const on = () => setFlag(true);
-    const off = () => setFlag(false);
-
-    const controls = {
-      toggle,
-      on,
-      off,
-    };
-
-    const state: State = {
-      [name]: flag,
-    } as any;
-
-    return { ...controls, ...state };
-
-    type State = {
-      [name in Name]: boolean;
-    };
-  };
+import { createUseFlag } from "./createUseFlag";
 
 const useFlag = createUseFlag(useReactState);
 
