0eace249449321543ff85675173b3c1063e7a21b
Aleksandr Šmailov <asa@solutionlab.net>
2020-12-03 09:34:12 +0200

    Added overload

--- createUseFlag.ts
+++ createUseFlag.ts
@@ -2,27 +2,43 @@ import { useState as useReactState } from "react";
 
 type UseState = typeof useReactState;
 
-export const createUseFlag = (useState: UseState) =>
-  function useFlag<Name extends string>(name: Name, initial: boolean) {
+export const createUseFlag = (useState: UseState) => {
+  function useFlag(initial?: boolean): [boolean, Controls];
+  function useFlag<Name extends string>(name: Name, initial?: boolean): State<Name> & Controls;
+  function useFlag<Name extends string>(nameOrInitial?: boolean | Name, initialOrNone?: boolean): any {
+    const initial = typeof nameOrInitial === 'boolean' ? nameOrInitial : (initialOrNone || false);
     const [flag, setFlag] = useState<boolean>(initial);
 
     const toggle = () => setFlag((value) => !value);
     const on = () => setFlag(true);
     const off = () => setFlag(false);
 
-    const controls = {
+    const controls: Controls = {
       toggle,
       on,
       off,
     };
 
-    const state: State = {
-      [name]: flag,
-    } as any;
+    if (typeof nameOrInitial === 'string')  {
+      const state: State<Name> = {
+        [nameOrInitial]: flag,
+      } as any;
 
-    return { ...controls, ...state };
+      return { ...controls, ...state };
+    }
 
-    type State = {
-      [name in Name]: boolean;
-    };
+    return [flag, controls];
   };
+
+  return useFlag;
+}
+
+export interface Controls {
+  toggle: () => void;
+  on: () => void;
+  off: () => void;
+}
+
+export type State<Name extends string> = {
+  [name in Name]: boolean;
+}
