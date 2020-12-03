f15c3c6e562b450d011dd3aaec21e0d629dcd1d0
Aleksandr Šmailov <asa@solutionlab.net>
2020-12-03 09:57:28 +0200

    Updated tests for named overload

--- __tests__/useFlag-test.ts
+++ __tests__/useFlag-test.ts
@@ -1,41 +1,91 @@
+import { useState } from "react";
 import { createUseFlag } from "../createUseFlag";
 
 describe("useFlag", () => {
-  it("updates value to 'true' when switching 'on'", () => {
-    const useFlag = createMockUseFlag("value", false);
+  let useFlag: ReturnType<typeof createUseFlag>;
+  beforeEach(() => {
+    useFlag = createUseFlag(getUseMockState());
+  })
 
-    const { value, on } = useFlag();
-    expect(value).toBe(false);
+  describe('named', () => {
+    test.each([
+      [undefined, false],
+      [false, false],
+      [true, true]
+    ])(`when provided with '%p' sets initial value to '%p'`, (intial, expected) => {
+      const { flagName } = useFlag('flagName', intial);
+      expect(flagName).toBe(expected);
+    });
 
-    on();
-    const { value: nextValue } = useFlag();
-    expect(nextValue).toBe(true);
-  });
+    it("updates value to 'true' when switching 'on'", () => {
+      const { flagName, on } = useFlag('flagName', false);
+      expect(flagName).toBe(false);
 
-  it("updates value to 'false' when switching 'off'", () => {
-    const useFlag = createMockUseFlag("value", true);
+      on();
+      const { flagName: nextValue } = useFlag('flagName');
+      expect(nextValue).toBe(true);
+    });
 
-    const { value, off } = useFlag();
-    expect(value).toBe(true);
+    it("updates value to 'false' when switching 'off'", () => {
+      const { flagName, off } = useFlag('flagName', true);
+      expect(flagName).toBe(true);
+  
+      off();
+      const { flagName: nextValue } = useFlag('flagName', true);
+      expect(nextValue).toBe(false);
+    });
 
-    off();
-    const { value: nextValue } = useFlag();
-    expect(nextValue).toBe(false);
-  });
+    it("switches on/off values when 'toggling' value", () => {
+      const { flagName, toggle } = useFlag('flagName', false);
+      expect(flagName).toBe(false);
+  
+      toggle();
+      expect(useFlag('flagName', false).flagName).toBe(true);
+  
+      toggle();
+      expect(useFlag('flagName', false).flagName).toBe(false);
+    });
+  })
 
-  it("switches on/off values when 'toggling' value", () => {
-    const useFlag = createMockUseFlag("value", false);
+  // it("updates value to 'true' when switching 'on'", () => {
+  //   const useFlag = createMockUseFlag("value", false);
 
-    const { value, toggle } = useFlag();
-    expect(value).toBe(false);
+  //   const { value, on } = useFlag();
+  //   expect(value).toBe(false);
 
-    toggle();
-    expect(useFlag().value).toBe(true);
+  //   on();
+  //   const { value: nextValue } = useFlag();
+  //   expect(nextValue).toBe(true);
+  // });
 
-    toggle();
-    expect(useFlag().value).toBe(false);
-  });
+  // it("updates value to 'false' when switching 'off'", () => {
+  //   const useFlag = createMockUseFlag("value", true);
+
+  //   const { value, off } = useFlag();
+  //   expect(value).toBe(true);
+
+  //   off();
+  //   const { value: nextValue } = useFlag();
+  //   expect(nextValue).toBe(false);
+  // });
+
+  // it("switches on/off values when 'toggling' value", () => {
+  //   const useFlag = createMockUseFlag("value", false);
+
+  //   const { value, toggle } = useFlag();
+  //   expect(value).toBe(false);
+
+  //   toggle();
+  //   expect(useFlag().value).toBe(true);
+
+  //   toggle();
+  //   expect(useFlag().value).toBe(false);
+  // });
 });
+
+function testOverload() {
+
+}
 
 function createMockUseFlag<T extends string>(name: T, initial: boolean) {
   let state: any = undefined;
@@ -58,4 +108,26 @@ function createMockUseFlag<T extends string>(name: T, initial: boolean) {
   const useFlag = createUseFlag(mockUseState as any);
 
   return () => useFlag(name, initial);
+}
+
+function getUseMockState(): typeof useState {
+  let state: any = undefined;
+  const setState = (update: any) => {
+    if (typeof update === "function") {
+      state = update(state);
+    } else {
+      state = update;
+    }
+  };
+  let firstTime = true;
+  const mockUseState = jest.fn((initial) => {
+    if (firstTime) {
+      state = initial;
+      firstTime = false;
+    }
+
+    return [state, setState];
+  });
+
+  return mockUseState as any;
 }
